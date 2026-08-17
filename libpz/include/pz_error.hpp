ddbc9b6f04dafe57c11fd56441454bafc40140fb
Dhivakar.S <cs25b023@smail.iitm.ac.in>
2026-08-17 18:57:51 +0530

    bitap algorithm implemented along with pz_error updates

--- libpz/include/pz_error.hpp
+++ libpz/include/pz_error.hpp
@@ -5,7 +5,7 @@
 namespace PzError {
 enum class PzErrorType;
 // other error / warning related functions or enums
-void report_error(PzErrorType type, const std::string &message) {
+inline void report_error(PzErrorType type, const std::string &message) {
   throw std::runtime_error("PzError " + message + " (Code: " +
                            std::to_string(static_cast<int>(type)) + ")");
 }
@@ -21,5 +21,30 @@ enum class PzError::PzErrorType {
   PZ_INVALID_ANALYSIS_TYPE,
   PZ_FILE_NOT_FOUND,
 };
+
+namespace fuzzy {
+
+class FileNotFoundError : public std::runtime_error {
+public:
+    explicit FileNotFoundError(std::string_view path) 
+    : std::runtime_error("Could not open file " + std::string(path)) {}
+};
+
+class EmptyDataError : public std::runtime_error {
+public:
+    EmptyDataError() : std::runtime_error("no data found in input file") {}
+};
+
+class InvalidQueryError : public std::runtime_error {
+public:
+    explicit InvalidQueryError(std::string_view reason) 
+    : std::runtime_error("Invalid Query " + std::string(reason)) {}
+};
+
+class LongPatternError : public std::runtime_error {
+public:
+    LongPatternError() : std::runtime_error("Pattern length is more than 63") {}
+};
+}
 
 #endif // PZ_ERROR_HPP
